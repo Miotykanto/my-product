a91f9919416ba0491746d28041275a66cd88b7fc
miotykanto <mirantotefy@gmail.com>
2019-03-14 15:20:57 +0300

    mise en prod

--- src/App.js
+++ src/App.js
@@ -5,26 +5,46 @@ import Tableau from './Component/tableau/tableau';
 import './App.css';
 import './Component/forme/forme.css'
 import './Component/tableau/tableau.css'
+import accounting from './Component/accounting';
 
 const App = () => {
+  
+  const initialFormState = { id: null, name: '', username: '' }
+  const [ currentUser, setCurrentUser ] = useState(initialFormState)
     const usersData = []
   
     const [ users, setUsers ] = useState(usersData)
     const [count, setCount] = useState(0);
-   
+    const [ editing, setEditing ] = useState(false)
+    
+    const updateUser = (id, updatedUser) => {
+      setEditing(false)
+    
+      setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+    }
+
     const ajouter = user => {
       user.id = count 
       setUsers([ ...users, user ])
     }
+
+    const somme = user => {
+      var tab = []
+      var x =0;
+      for (let i=0;i<users.length;i++) {
+        tab[i] = users[i].prix
+        x=parseInt(x)+parseInt(tab[i])
+      }
+      document.getElementById("somme").innerHTML = 'TOTAL = ' + accounting.formatMoney (x);
+    }    
 
     const deleteUser = id => {
         setUsers(users.filter(user => user.id !== id))
     }
 
   const editRow = user => {
-    console.log(this.user.prix);
-    
-    setUsers({ id: user.id, prod: user.prod, prix: user.prix })
+    setEditing(true)
+    setCurrentUser({ id: user.id, prod: user.prod, prix : user.prix })
   }
 
     return (
@@ -35,7 +55,7 @@ const App = () => {
             <Form ajouter={ajouter} setCount={setCount} count={count}/>
           </div>
           <div className="flex-large">
-            <Tableau users={users} deleteUser={deleteUser} editRow={editRow}/>
+            <Tableau users={users} deleteUser={deleteUser} editRow={editRow} editing={editing} setEditing={setEditing} updateUser={updateUser} somme={somme}/>
           </div>
         </div>
 
--- src/Component/tableau/tableau.jsx
+++ src/Component/tableau/tableau.jsx
@@ -1,88 +1,110 @@
 import { confirmAlert } from 'react-confirm-alert'; // Import
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import 'react-confirm-alert/src/react-confirm-alert.css' // Import css
 
-class confirm extends Component {
-    submit = (a) => {
-      confirmAlert({
-        customUI: ({ onClose }) => {
-          return (  
-            <div className="custom-ui" id="popup">
-              <h6>Suppression Produit</h6><br/><br/>
-            <center>
-              <button className="btn btn-dark"
-                onClick={() => {
-                  this.props.deleteUser(a);
-                  onClose();
-                }}
-              >
-                OUI
-              </button><a>&nbsp;&nbsp;</a>
-              <button  className="btn btn-dark" onClick={onClose}>NON</button>
-              </center>
-            </div>
-          );
-        }
-      })
-    };
+const confirm = props => {
 
-    submit1 = (a) => {
-        confirmAlert({
-            customUI: ({ onClose }) => {
-              return (  
-               
-                <div className="custom-ui" id="popup">
-                  <input id="entree" className="modif"/><br/><br/>
-                  <div id="erreur"></div><br/>
-                <center>
-                  <button className="btn btn-dark"
-                    onClick={() => {
-                      if (isNaN(document.getElementById("entree").value)) {
-                        var valid="Entrer un nombre";
-                        document.getElementById('erreur').innerHTML= valid;
-                     } else {
-                        var valid="";
-                        document.getElementById('erreur').innerHTML= valid;
-                        this.editRow(a);
-                        
-                      }
-                    }}
-                  >
-                    Ok
-                  </button><a>&nbsp;&nbsp;</a>
-                  <button  className="btn btn-dark" onClick={onClose}>Annuler</button>
-                  </center>
-                </div>
-              );
-            }
-          })
-    };
-      render(){
+  const initial = { id: null, prod: '', prix: '' }
+
+  const [ user, setUser ] = useState(initial)
+        const changed = event => {
+
+           const { name, value } = event.target
+        
+           setUser({ ...user, [name]: value })
+     }
+
             return (
-                <center>
-                    <table class="table table-bordered">
+                <center onSubmit={event => {
+                  event.preventDefault()
+                }}>
+                    <table className="table table-bordered">
                     <thead>
                         <tr className="tab">
-                            <th>id</th>
+                            <th>Id</th>
                             <th>Produits</th>
                             <th>Prix</th>
                             <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {this.props.users.length > 0 ? (
-                        this.props.users.map(user => (
+                        {props.users.length > 0 ? (
+                        props.users.map(user => (
                                 <tr key={user.id}>
                                     <td>{user.id}</td>
                                     <td>{user.prod}</td>
-                                    <td id="prix">{user.prix}</td>
+                                    <td id="prix" onChange={changed}>{user.prix}</td>
                                     <td>
-                                        <button className="btn btn-danger" onClick={()=>this.submit(user.id)}>X</button>
-                                        <a>&nbsp;</a><button type="submit" className="btn btn-success"
+                      {/* SUPPRESSION */}
+                                        <button className="btn btn-danger"
+                                        onClick={ () => {
+                                          confirmAlert({
+                                            customUI: ({ onClose }) => {
+                                              return (  
+                                                <center>
+                                                <div className="custom-ui" id="popup">
+                                                  <h6>Suppression Produit</h6><br/>
+                                                  <h6>{user.prod}</h6><br/>
+                                                  <button className="btn btn-dark"
+                                                    onClick={() => {
+                                                      props.deleteUser(user.id);
+                                                      onClose();
+                                                    }}
+                                                  >
+                                                    OUI
+                                                  </button><a>&nbsp;&nbsp;</a>
+                                                  <button  className="btn btn-dark" onClick={onClose}>NON</button>
+                                                 </div>
+                                                 </center>
+                                              );
+                                            }
+                                          })
+                                        }
+                                        }
+                                        >X</button>
+                                        <a>&nbsp;</a>
+                        {/* MODIFICATION */}
+                                        <button className="btn btn-success"
                                           onClick={() => {
-                                          console.log(user.prix);
-                                          this.submit1(user.prix)
-                                         }}>Edit</button>
+                                            confirmAlert({
+                                                customUI: ({ onClose }) => {
+                                                  return (  
+                                                    <form id='ID_FORMULAIRE'>
+                                                      <center>
+                                                        <div className="custom-ui" id="popup">
+                                                          <input name='inputStoreID' placeholder={ user.prix } id="entree" className="modif"></input><br/>
+                                                          <p id="e"></p><br/>
+                                                                                                              
+                                                          <button className="btn btn-dark"
+                                                          onClick={(e) => {
+                                                            // var valid="Entrer un nombre";
+                                                            var test = document.forms['ID_FORMULAIRE'].elements['inputStoreID'].value
+                                                              if (isNaN(test) || test=="") {
+                                                                var valid = "Entrer un Nombre"
+                                                                e.preventDefault()
+                                                                document.getElementById('e').innerHTML= valid;
+                                                              } else {
+                                                                valid="";
+                                                                document.getElementById('e').innerHTML= valid;
+                                                                user.prix = document.forms['ID_FORMULAIRE'].elements['inputStoreID'].value;
+                                                                props.updateUser(user.prix, user)
+                                                                props.editRow(user.id);
+                                                                onClose();
+                                                              }
+                                                            }
+                                                          } 
+                                                          >OK</button><a>&nbsp;&nbsp;</a>
+
+                                                          <button  className="btn btn-dark" onClick={onClose}>Annuler</button>
+                                                        </div>
+                                                      </center>
+                                                    </form>
+                                                  );
+                                                }
+                                              })
+                                        }
+                                      }
+                                         >Edit</button>
                                     </td>
                                 </tr>
                             ))
@@ -93,16 +115,11 @@ class confirm extends Component {
                         )}
                     </tbody>
                 </table>
-                <button className="btn btn-warning" onClick={ () => {
-                  console.log(this.user.prix);
-                  
-                  var i=this.user.prix
-                  document.getElementById("somme").innerHTML=i
-                }}>TOTAL</button>
-                <div id="somme"></div>
+                <button className="btn btn-warning" onClick={ () => {props.somme(user)}}>TOTAL</button><br/><br/>
+                <div id="somme" display="none">TOTAL = </div>
                 </center>	
             );
       }
-}
+
 	
 export default confirm;
